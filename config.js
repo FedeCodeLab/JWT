405daef4ca38c1665903016493b1700c4f758787
FedeCodeLab <federicoguzman.css@gmail.com>
2025-11-26 19:33:10 -0300

    jwt y cookies

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-export const { PORT = 3000 } = process.env;
+export const {
+  PORT = 3000,
+  SALT_ROUNDS = 10,
+  SECRET_JWT_KEY = "this-is-an-awesome-secret-key-large/q/@we!23",
+} = process.env;
--- user-repository.js
+++ user-repository.js
@@ -1,6 +1,8 @@
 import DBLocal from "db-local";
 const { Schema } = new DBLocal({ path: "./db" });
 import crypto from "crypto";
+import { SALT_ROUNDS } from "./config.js";
+import bcrypt from "bcrypt";
 
 const User = Schema("user", {
   _id: { type: String, required: true },
@@ -9,30 +11,53 @@ const User = Schema("user", {
 });
 
 export class userRepository {
-  static create({ username, password }) {
-    if (typeof username !== "string")
-      throw new Error("username must be a string");
-    if (username.length < 3)
-      throw new Error("username must be at least 3 characters long");
-
-    if (typeof password !== "string")
-      throw new Error("password must be a string");
-    if (password.length < 6)
-      throw new Error("password must be at least 6 characters long");
+  static async create({ username, password }) {
+    Validation.username(username);
+    Validation.password(password);
 
     const user = User.findOne({ username });
     if (user) throw new Error("username already exists");
 
     const id = crypto.randomUUID();
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
     User.create({
       _id: id,
       username,
-      password,
+      password: hashedPassword,
     }).save();
 
     return id;
   }
 
-  static login({ username, password }) {}
+  static async login({ username, password }) {
+    Validation.username(username);
+    Validation.password(password);
+
+    const user = User.findOne({ username });
+    if (!user) throw new Error("username does not exist");
+
+    const isValid = await bcrypt.compareSync(password, user.password);
+    if (!isValid) throw new Error("password is invalid");
+
+    const { password: _, ...publicUser } = user;
+
+    return publicUser;
+  }
+}
+
+class Validation {
+  static username(username) {
+    if (typeof username !== "string")
+      throw new Error("username must be a string");
+    if (username.length < 3)
+      throw new Error("username must be at least 3 characters long");
+  }
+
+  static password(password) {
+    if (typeof password !== "string")
+      throw new Error("password must be a string");
+    if (password.length < 6)
+      throw new Error("password must be at least 6 characters long");
+  }
 }
